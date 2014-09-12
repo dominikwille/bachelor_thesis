6f0b380779f78a0c9918c54da43d3942517ac1dc
Dominik Wille <dominik.wille@erdfisch.de>
2014-09-12 19:23:12 +0200

    fitting

--- simulation/bachelor_thesis/simulations/correlation/__main__.py
+++ simulation/bachelor_thesis/simulations/correlation/__main__.py
@@ -70,51 +70,76 @@ class Plates1dNoCrossing(Langevin):
             c = 0
         return c
 
-num = 1
-max_steps = 10000000
-# correlation = numpy.array([0.0]*max_steps)
-correlation_x_min = 0
-correlation_x_max = 300
-correlation_precision = 100000
-correlation = numpy.array([0.0]*(correlation_x_max - correlation_x_min))
-x = numpy.array(range(correlation_x_min, correlation_x_max))
+# num = 1
+# max_steps = 10000000
+# # correlation = numpy.array([0.0]*max_steps)
+# correlation_x_min = 0
+# correlation_x_max = 300
+# correlation_precision = 100000
+# correlation = numpy.array([0.0]*(correlation_x_max - correlation_x_min))
+# x = numpy.array(range(correlation_x_min, correlation_x_max))
+#
+#
+# # for j in range(100):
+# #     print j
+# #     for i in range(num):
+# #         test = Plates1dNoCrossing()
+# #         test.absorbed = True
+# #         test.max_steps = max_steps
+# #         test.r = numpy.array([-10.0])
+# #         x, y = test.info_walk()
+# #         correlation += numpy.array(y)
+# # correlation /= float(num*100)
+#
+# test = Plates1dNoCrossing()
+# test.absorbed = True
+# test.max_steps = max_steps
+# test.r = numpy.array([-10.0])
+# steps, y = test.info_walk()
+#
+# for i in range(correlation_x_max - correlation_x_min):
+#     print i
+#     j = 0
+#     j_index = 0
+#     while j < correlation_precision:
+#         if y[j_index] == 1:
+#             j += 1
+#             if y[j_index + i + correlation_x_min] == 1:
+#                 correlation[i] += 1
+#         j_index +=1
+#
+# correlation /= float(correlation_precision)
+
+import csv
+
+c_aa = []
+c_ab = []
+xdata = []
+i = 0.0
+with open('data/test.csv', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        xdata.append(i)
+        c_aa.append(float(row[0]))
+        c_ab.append(float(row[1]))
+        i += 1.0
+
+from scipy.optimize import curve_fit
+
+def func(x_, a, b, c, d, e, f, g):
+    y = a * numpy.exp(-b * x_) + c * numpy.exp(-d * x_) + e * numpy.exp(-f * x_) + g
+    return y
+
+def func2(x_, a, b, c):
+    return c * (1-numpy.exp(-a * (x_ + b)))
+
+popt, pcov = curve_fit(func2, xdata, c_ab)
 
 
-# for j in range(100):
-#     print j
-#     for i in range(num):
-#         test = Plates1dNoCrossing()
-#         test.absorbed = True
-#         test.max_steps = max_steps
-#         test.r = numpy.array([-10.0])
-#         x, y = test.info_walk()
-#         correlation += numpy.array(y)
-# correlation /= float(num*100)
 
-test = Plates1dNoCrossing()
-test.absorbed = True
-test.max_steps = max_steps
-test.r = numpy.array([-10.0])
-steps, y = test.info_walk()
-
-for i in range(correlation_x_max - correlation_x_min):
-    print i
-    j = 0
-    j_index = 0
-    while j < correlation_precision:
-        if y[j_index] == 1:
-            j += 1
-            if y[j_index + i + correlation_x_min] == 1:
-                correlation[i] += 1
-        j_index +=1
-
-correlation /= float(correlation_precision)
-
-
-
-print correlation
-print y
-plt.plot(x, correlation)
+plt.plot(xdata, c_aa)
+plt.plot(xdata, c_ab)
+plt.plot(xdata, func2(numpy.array(xdata), *popt))
 plt.xlabel('Step')
 plt.ylabel('Posibilty to be absorbed')
 plt.ylim([0, 1])
